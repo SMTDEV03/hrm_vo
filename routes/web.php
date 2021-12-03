3f41fc0e3a34c2144ece37db66ea92fe5a2092dc
ashish <ashish@gmail.com>
2021-12-03 17:56:32 +0530

    ashish

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('addleave', [LeaveController::class, 'add_leaves_type'])->name('admin.addleave');
         Route::post('leavetypebyID/{id?}', [LeaveController::class, 'leavetypebyID'])->name('admin.editleave');
         Route::get('deleteleave/{id?}', [LeaveController::class, 'LeavetypeDelet'])->name('admin.deleteleave');
-        Route::get('leaveapprove', [LeaveController::class, 'leaveapplication'])->name('pages.leaveapprove');
+        //Route::get('leaveapprove', [LeaveController::class, 'leaveapplication'])->name('pages.leaveapprove');
         Route::get('allholidays', [HolidayController::class, 'index'])->name('pages.holidays');
         Route::post('addholiday', [HolidayController::class, 'add_holiday'])->name('pages.addholiday');
         Route::post('holidaybyID/{id?}', [HolidayController::class, 'holidaybyID'])->name('admin.editholiday');
@@ -58,10 +58,10 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('/profile', [HomeController::class, 'user_profile'])->name('pages.profile');
         Route::post('/profileUpdate', [HomeController::class, 'updateProfile'])->name('profileUpdate');
         Route::get('allholidays', [HolidayController::class, 'index'])->name('pages.holidays');
-        Route::get('leaveapprove', [LeaveController::class, 'leaveapplication'])->name('pages.leaveapprove');
+        //Route::get('leaveapprove', [LeaveController::class, 'leaveapplication'])->name('pages.leaveapprove');
        
     });
-
+    Route::get('leaveapprove', [LeaveController::class, 'leaveapplication'])->name('pages.leaveapprove');
     Route::get('/auth/logout', [MainController::class, 'logout'])->name('auth.logout');
 
 });
